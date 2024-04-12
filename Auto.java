f7f47d941f7955f194ed2d40a9e0d7e273264a88
AlessiaBonazza <164646655+AlessiaBonazza@users.noreply.github.com>
2024-04-12 12:56:21 +0200

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -9,23 +9,53 @@ public static void main(String[] args) {
 
    
     Scanner macchina = new Scanner(System.in); 
-    System.out.println("inserisci modello auto da inserire");
+    
+    System.out.println("che specifiche dell'auto conosci? \n1-modello+colore+anno; \n2-modello e anno di costruzione; \n3-nessuna");
+    int scelta = macchina.nextInt();
+    System.out.println("inserisci modello auto da inserire o x se non si conoscono le specifiche\n");
     String modello = macchina.nextLine();
-    System.out.println("inserisci colore auto da inserire");
+
+    System.out.println("inserisci colore auto da inserire o x se non si conoscono le specifiche\n");
     String colore = macchina.nextLine();
-    System.out.println("inserisci anno auto da inserire");
+    System.out.println("inserisci anno auto da inserire o x se non si conoscono le specifiche\n");
     int anno = macchina.nextInt();
+
+
+    //inserire condizione nelle quali si chiede quali parametri si vogliono inserire. Generico=macchina2 
 
 
     Auto macchina1 = new Auto(modello, colore, anno);
     Auto macchina2 = new Auto();
-    Auto macchina3 = new Auto(nome,anno);
+    Auto macchina3 = new Auto(modello,anno);
 
         // Stampare le informazioni dell'auto
+
+        if (scelta==1) { 
+        
         System.out.println("Auto inserita:");
         System.out.println("Modello: " + macchina1.getModello());
         System.out.println("Colore: " + macchina1.getColore());
         System.out.println("Anno di produzione: " + macchina1.getAnno());
+    }
+        else if (scelta==2) { 
+        
+        System.out.println("Auto inserita:");
+        System.out.println("Modello: " + macchina3.getModello());
+        System.out.println("Colore: " + macchina3.getColore());
+        System.out.println("Anno di produzione: " + macchina3.getAnno());
+    }
+        else if (scelta==3) { 
+        
+        System.out.println("Auto inserita:");
+        System.out.println("Modello: " + macchina2.getModello());
+        System.out.println("Colore: " + macchina2.getColore());
+        System.out.println("Anno di produzione: " + macchina2.getAnno());
+    }
+
+        else { 
+            System.out.println ("Valore di scelta non valido");   
+        }
+
 
        
 
